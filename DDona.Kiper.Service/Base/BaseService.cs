e9d49cbe8436643c6c5d375eec655de3a8e029ba
Diego Doná <diedona@gmail.com>
2019-09-23 23:49:44 -0300

    base service with IBaseEntity

---
+++ DDona.Kiper.Service/Base/BaseService.cs
@@ -0,0 +1,68 @@
+﻿using DDona.Kiper.Domain.DomainDefinition;
+using DDona.Kiper.Infra;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DDona.Kiper.Service.Base
+{
+    public abstract class BaseService<T> : IBaseService<T> where T : class, IBaseEntity
+    {
+        protected readonly KiperDesafioContext _db;
+        protected DbSet<T> _dbSet => _db.Set<T>();
+
+        public BaseService(KiperDesafioContext db)
+        {
+            _db = db;
+            _db.Set<T>();
+        }
+
+        public virtual void Save(T entity)
+        {
+            _dbSet.Add(entity);
+            _db.SaveChanges();
+        }
+
+        public virtual void Update(T entity)
+        {
+            _dbSet.Update(entity);
+            _db.SaveChanges();
+        }
+
+        public virtual void Delete(int id)
+        {
+            T entity = _db.Set<T>().Find(id);
+            if (entity == null)
+            {
+                return;
+            }
+
+            entity.Status = false;
+            _db.SaveChanges();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            entity.Status = false;
+            _dbSet.Update(entity);
+            _db.SaveChanges();
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public virtual T Get(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public bool Exists(Expression<Func<T, bool>> condition)
+        {
+            return _dbSet.Any(condition);
+        }
+    }
+}
---
+++ DDona.Kiper.Service/Base/IBaseService.cs
@@ -0,0 +1,18 @@
+﻿using DDona.Kiper.Domain.DomainDefinition;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DDona.Kiper.Service.Base
+{
+    public interface IBaseService<T> where T: class, IBaseEntity
+    {
+        void Save(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        void Delete(T entity);
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        bool Exists(Expression<Func<T, bool>> condition);
+    }
+}
